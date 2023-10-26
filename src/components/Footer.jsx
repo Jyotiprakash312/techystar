2cda243ad80197ac6f0e901d57e1affb51ec6d42
JYOTIPRAKASH MOHANTA <jyotiprakashjjj@gmail.com>
2023-10-26 23:51:55 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/jyotiprakash-mohanta-447573199/" target={"blank"}>
+            Linkedin
+          </a>
+          <a href="https://www.instagram.com/jyotiprakash524/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://www.github.com/Jyotiprakash312" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
